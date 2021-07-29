4193c4f5f74fd745c21b36f6a3baeb6628532e34
omarabdiwali <omarabdiwali17@gmail.com>
2021-07-29 01:08:30 -0600

    Changed scripts.

--- src/components/displayBestSellers.js
+++ src/components/displayBestSellers.js
@@ -2,36 +2,28 @@ import React from 'react';
 import { SimpleGrid, Spinner } from '@chakra-ui/react';
 import Books from './displayBooks';
 
-export default class BestSellers extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiKey: process.env.REACT_APP_NYT_API_KEY, books: '' };
-  }
+export default function BestSellers() {
+  const apiKey = process.env.REACT_APP_NYT_API_KEY;
+  const [books, setBooks] = React.useState('');
 
-  async componentDidMount() {
-    const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${this.state.apiKey}`;
-    const response = await fetch(url);
-    let data = await response.json();
-    data = data.results.books;
+  React.useEffect(() => {
+    const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`;
+    fetch(url).then(resp => resp.json()).then(data => setBooks(data.results.books)).catch(err => console.error(err));
+  }, [apiKey])
 
-    this.setState({ books: data });
-  }
-
-  render() {
-    return (
-      <div>
-        {!this.state.books ? (
-          <center>
-            <Spinner />
-          </center>
-        ) : (
-          <SimpleGrid columns={window.innerWidth > 800 ? 4 : window.innerWidth > 500 ? 2 : 1}>
-            {this.state.books.map((book, i) => {
-              return <Books book={book} type={'nyt'} key={i} />;
-            })}
-          </SimpleGrid>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {!books ? (
+        <center>
+          <Spinner />
+        </center>
+      ) : (
+        <SimpleGrid columns={window.innerWidth > 800 ? 4 : window.innerWidth > 500 ? 2 : 1}>
+          {books.map((book, i) => {
+            return <Books book={book} type={'nyt'} key={i} />;
+          })}
+        </SimpleGrid>
+      )}
+    </div>
+  )
 }
--- src/components/displaySearch.js
+++ src/components/displaySearch.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SimpleGrid } from '@chakra-ui/react';
 import Books from './displayBooks';
 
 export default function Search({ data }) {
+  const [size, setSize] = useState(window.innerWidth > 800 ? 4 : window.innerWidth > 500 ? 2 : 1);
+
+  window.addEventListener('resize', () => {
+    window.innerWidth > 800 ? setSize(4) : window.innerWidth > 500 ? setSize(2) : setSize(1);
+  })
+
   return (
-    <SimpleGrid columns={window.innerWidth > 800 ? 4 : window.innerWidth > 500 ? 2 : 1}>
+    <SimpleGrid columns={size}>
       {data.map((book, i) => {
         return (<Books book={book} type={"google"} key={i} />)
       })}
